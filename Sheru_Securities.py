dcb7552ea305182439c91f0d2dbfa9ce3bea4ad2
Harmohit Singh <harmohitsingh05@gmail.com>
2018-06-15 21:13:54 +0530

    authentication message

--- Sheru_Securities.py
+++ Sheru_Securities.py
@@ -1,13 +1,11 @@
 known_users = ["Harman","Snimar", "Antar", "Sehaj", "Ajay", "Armaan", "Jazz"]
 
-print(len(known_users))
+
 
 while True:
     print("Hi! I'm Sheru! Security guard (^.^)")
-    name = input("What is your name?: ").strip()
-
+    name = input("What is your name?: ").strip().capitalize()
     if name in known_users:
-        print("Hello {}. You are authorised to enter the Dark zone.").format(name)
+        print("Hello {}. You are authorised to enter the Dark zone.".format(name))
     else:
-        print("Your name is not recognised. Get out before I throw you outta here")
-        
+        print("Your name is not recognised. Get out of here!! Before I throw you outta here")
