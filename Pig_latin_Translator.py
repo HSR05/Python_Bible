883653e72a25acce2d7d418aecc896762a6ac0ab
Harmohit Singh <harmohitsingh05@gmail.com>
2018-06-17 13:24:43 +0530

    final commit with all functions of translation

--- Pig_latin_Translator.py
+++ Pig_latin_Translator.py
@@ -15,12 +15,22 @@ for word in words:
     if word[0] in "aeiou":
         new_word = word+"yay"
         new_words.append(new_word)
-print(new_words)
-
-
-#otherwise, move the first consonant cluster to end and add "ay"
+    else:
+        vowel_pos = 0
+        for letter in word:
+            if letter not in "aieou":
+                vowel_pos = vowel_pos + 1
+            else:
+                break
+        cons=word[:vowel_pos]    
+        the_rest = word[vowel_pos:]
+        new_word = the_rest + cons + "ay"
+        new_words.append(new_word)
 
 #stick words back together
 
+output = " ".join(new_words)
 
 #output the final string
+
+print(output)
